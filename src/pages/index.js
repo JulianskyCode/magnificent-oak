f3c009bc66f51c364132f04f4ba5d8f1abb7b49a
Ban Iulian <ban.iulian.j@gmail.com>
2021-02-21 22:17:14 +0000

    index.js: updated Paragraph in div

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,8 +56,9 @@ Involved in the recruitment process, and the management of existing employees.
               <h3 className="mb-0">Team Leader Customer Support</h3>
               <div className="subheading mb-3">Hootsuite, Bucharest</div>
               <p>
-                Monitored the performance of the customer support team members and worked to increase team morale and motivation. &lt;br /&gt;
-<br />
+                Monitored the performance of the customer support team members and worked to increase team morale and motivation.
+
+&lt;br /&gt;
 dadas
               </p>
             </div>
