b7d8b74e765bdaef5e2ae4ac758e750f61be0a05
Stackbit Code Editor <support@stackbit.com>
2021-02-21 23:15:41 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -233,7 +233,7 @@ Involved in the recruitment process, and the management of existing employees.
               <i className="fab fa-jira"></i>
             </li>
             <li className="list-inline-item">
-              <i className="fab fa-css3-alt"></i>
+              <i className="fab fa-zendesk"></i>
             </li>
             <li className="list-inline-item">
               <i className="fab fa-js-square"></i>
