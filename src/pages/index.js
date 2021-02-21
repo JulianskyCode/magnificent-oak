06df80ba8e7a7910c2e5eb35c7a2e12542a6aec9
Stackbit Code Editor <support@stackbit.com>
2021-02-21 23:45:31 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -292,9 +292,7 @@ Involved in the recruitment process, and the management of existing employees.
           </p>
           <p className="mb-0">
             When forced indoors, I follow a number of sci-fi and fantasy genre
-            movies and television shows, I am an aspiring chef, and I spend a
-            large amount of my free time exploring the latest technology
-            advancements in the front-end web development world.
+            movies and television shows, I am an aspiring chef,
           </p>
         </div>
       </section>
