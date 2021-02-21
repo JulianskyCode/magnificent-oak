16c8a9175d0bb2053f635051abd61a6394b060c6
Stackbit Code Editor <support@stackbit.com>
2021-02-21 22:37:03 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -132,11 +132,7 @@ Involved in the recruitment process, and the management of existing employees.
               <h3 className="mb-0">Junior Web Designer</h3>
               <div className="subheading mb-3">Shout! Media Productions</div>
               <p>
-                Podcasting operational change management inside of workflows to
-                establish a framework. Taking seamless key performance
-                indicators offline to maximise the long tail. Keeping your eye
-                on the ball while performing a deep dive on the start-up
-                mentality to derive convergence on cross-platform integration.
+                Line;
               </p>
             </div>
             <div className="resume-date text-md-right">
