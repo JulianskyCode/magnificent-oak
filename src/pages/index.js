669609e2c70831908f456b991976dc89b24703c4
Stackbit Code Editor <support@stackbit.com>
2021-02-21 23:33:23 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -253,7 +253,7 @@ Involved in the recruitment process, and the management of existing employees.
             </li>
             <li>
               <i className="fa-li fa fa-check"></i>
-              Trouble solver
+              Problem solver
             </li>
             <li>
               <i className="fa-li fa fa-check"></i>
