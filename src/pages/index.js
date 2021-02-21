a57c8f890dfa43c3a464171a7c3a966c824a1866
Stackbit Code Editor <support@stackbit.com>
2021-02-21 23:14:31 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -230,7 +230,7 @@ Involved in the recruitment process, and the management of existing employees.
           </div>
           <ul className="list-inline dev-icons">
             <li className="list-inline-item">
-              <i className="fab fa-html5"></i>
+              <i className="fab fa-zendesk"></i>
             </li>
             <li className="list-inline-item">
               <i className="fab fa-css3-alt"></i>
