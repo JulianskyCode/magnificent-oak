9e5b32300ddcecb67c58c794c144d708ed6b0517
Stackbit Code Editor <support@stackbit.com>
2021-02-21 23:40:39 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -320,24 +320,6 @@ Involved in the recruitment process, and the management of existing employees.
               <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
               Hootsuite Platform Certification, Hootsuite Academy
             </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Adobe Creative Jam 2008
-              (UI Design Category)
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>2<sup>nd</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2008
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - James Buchanan High School - Hackathon 2006
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>3<sup>rd</sup>
-              Place - James Buchanan High School - Hackathon 2005
-            </li>
           </ul>
         </div>
       </section>
