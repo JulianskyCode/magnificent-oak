8a7a124cbb5b1759ea77c5687f28c5b2f4bb70c0
Stackbit Code Editor <support@stackbit.com>
2021-02-21 21:49:51 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
-  firstName: 'Anubhav',
+  firstName: 'Elena',
   lastName: 'Srivastava',
   // social
   socialLinks: [
